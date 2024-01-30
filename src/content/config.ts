037d3a7e512386f6a214ff0d6ddb6449b004ad13
Parsifa1 <li.aldric@gmail.com>
2024-01-31 00:25:53 +0800

    fix:修复modDatetime

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,8 @@
-import { SITE } from "@config";
-import { defineCollection, z } from "astro:content";
+import { SITE } from '@config'
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-  type: "content",
+  type: 'content',
   schema: ({ image }) =>
     z.object({
       author: z.string().default(SITE.author),
@@ -11,16 +11,16 @@ const blog = defineCollection({
       title: z.string(),
       featured: z.boolean().optional(),
       draft: z.boolean().optional(),
-      tags: z.array(z.string()).default(["others"]),
+      tags: z.array(z.string()).default(['others']),
       ogImage: image()
-        .refine(img => img.width >= 1200 && img.height >= 630, {
-          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        .refine((img) => img.width >= 1200 && img.height >= 630, {
+          message: 'OpenGraph image must be at least 1200 X 630 pixels!',
         })
         .or(z.string())
         .optional(),
       description: z.string(),
       canonicalURL: z.string().optional(),
     }),
-});
+})
 
-export const collections = { blog };
+export const collections = { blog }
