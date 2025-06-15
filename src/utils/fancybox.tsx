feb2887ac250f88e55c361c3ef8739a44333252d
Parsifa1 <li.aldric@gmail.com>
2025-06-16 02:42:39 +0800

    feat(fancybox): add img preview

---
+++ src/utils/fancybox.tsx
@@ -0,0 +1,17 @@
+import { Fancybox } from "@fancyapps/ui";
+import { useEffect } from "react";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+export default function ImageZoomer() {
+  useEffect(() => {
+    Fancybox.bind("article img", {
+      groupAll: true,
+      mainClass: "fancybox-custom",
+    });
+    return () => {
+      Fancybox.destroy();
+    };
+  }, []);
+
+  return <></>;
+}
