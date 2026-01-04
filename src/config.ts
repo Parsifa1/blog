7ee24c71d8fa97ab2bd1ba004f48eeac6c08a267
Parsifa1 <li.aldric@gmail.com>
2026-01-04 10:49:38 +0000

    feat: change postPerPage

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const SITE: Site = {
   title: "Parsifal's Blog",
   ogImage: "https://i2.woh.to/2023/09/30/photo_2023-05-07_23-49-04a122368bf889ed18.jpg",
   lightAndDarkMode: true,
-  postPerPage: 4,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
 };
