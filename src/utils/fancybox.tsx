f8d9c7dd4c01d249ff3e78c1bf85dbf86a7583ae
Parsifa1 <li.aldric@gmail.com>
2026-01-18 18:20:10 +0000

    feat(optmize): lazyload for fancybox

--- src/utils/fancybox.tsx
+++ src/utils/fancybox.tsx
@@ -1,6 +1,6 @@
 import { Fancybox } from "@fancyapps/ui";
 import { useEffect } from "react";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+// import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 export default function ImageZoomer() {
   useEffect(() => {
